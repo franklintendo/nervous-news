dfc86eb167aadd344f28a0ee494ea191d22bd642
Frank Moran <info@francisfrankmoran.com>
2020-10-25 16:50:52 -0400

    client src init

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const router = require("express").Router();
 router.get("/reports", (req, res) => {
 
     const queryURL = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_KEY}`;
-  console.log("test");
-    axios.get(queryURL)
+  
+    axios
+    .get(queryURL)
     .then((report) => {
-      console.log(report.data);
-            res.send(report.data);
+      res.send(report.data);
     })
-    .catch(err => res.send("error"));
+    .catch(err => console.log(err));
 
 });
 
