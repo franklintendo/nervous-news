bd6dcc69dbe0eb57f5f2a3f76b605b5817701320
Frank Moran <info@francisfrankmoran.com>
2020-10-26 15:18:43 -0400

    speech synth

--- client/src/components/Reports.js
+++ client/src/components/Reports.js
@@ -18,9 +18,14 @@ function Reports() {
         .catch((err) => console.log(err));
     }
 
+    function sayReportTitle(title) {
+        const to_speak = new SpeechSynthesisUtterance(title);
+        window.speechSynthesis.speak(to_speak);
+    }
+
     function renderReportLinks(reports) {
         return reports.map((report, index) => {
-            return <div key={index}><img src={report.urlToImage} style={{width: "250px", height: "auto"}} /> <a href={report.url}>{report.title}</a> </div>
+            return <div key={index}><img src={report.urlToImage} style={{width: "250px", height: "auto"}} /> <a href={report.url}>{report.title}</a> <span onClick={() => sayReportTitle(report.title)}>Play Speech</span>  </div>
         });
     }
 
