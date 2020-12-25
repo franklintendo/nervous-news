31ac2b8ab849cf8fa1211de0163f3cc8e42bf15c
Frank Moran <info@francisfrankmoran.com>
2020-12-25 16:03:13 -0500

    report captions

--- client/src/pages/Bulletin.js
+++ client/src/pages/Bulletin.js
@@ -13,17 +13,37 @@ function Bulletin() {
         title_speak.pitch = 1;
         description_speak.pitch = 1.5;
         content_speak.pitch = 2;
+
+        title_speak.onstart = function(event) {
+            document.getElementById('report-caption').innerHTML = event.utterance.text;
+          };
+        
+        description_speak.onstart = function(event) {
+            document.getElementById('report-caption').innerHTML = event.utterance.text;
+          };
+        content_speak.onstart = function(event) {
+            document.getElementById('report-caption').innerHTML = event.utterance.text;
+          };
+
         window.speechSynthesis.speak(title_speak);
         window.speechSynthesis.speak(description_speak);
         window.speechSynthesis.speak(content_speak);
     }
 
+    // let report = state.currentReport.description + " " + state.currentReport.content;
+    
+    // let firstPart = report.substring(0, report.length / 4);
+    // let firstPart = report.substring(0, report.length / 4);
+    // console.log(report);
+
     return(
         <div>
+            <img src={state.currentReport.image} alt={state.currentReport.title} style={{width: "400px", height: "auto"}} />
             {sayReportTitle(state.currentReport)}
-            <p>{state.currentReport.title}</p>
+            {/* <p>{state.currentReport.title}</p>
             <p>{state.currentReport.description}</p>
-            <p>{state.currentReport.content}</p>
+            <p>{state.currentReport.content}</p> */}
+            <p id="report-caption"></p>
         </div>
     )
 }
